29fa110b868af4ddf917936fc2e598accdafbef6
Induwara Dilhara <164741563+InduwaraSH@users.noreply.github.com>
2026-05-09 14:26:57 +0530

    Revert "feature/home_page_func_adding"
    This reverts commit 3fff7cd8805340befbffd4ca7f65cd47e1a6f6f6.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:bingo/Common/auth_checker.dart';
+import 'package:bingo/Common/Logging.dart';
+import 'package:bingo/Driver/Dri_Nav_Bar.dart';
 import 'package:bingo/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
@@ -13,11 +14,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthChecker(),
-    );
+    return MaterialApp(home: Logging());
   }
 }
